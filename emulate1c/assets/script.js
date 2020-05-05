107ca6729db83255e1c9106cfd954296ba0c4828
Dreamod <dreamod1@gmail.com>
2020-05-05 18:55:08 +0300

    Fix problem with token and source checking
    Signed-off-by: Dreamod <dreamod1@gmail.com>

--- emulate1c/assets/script.js
+++ emulate1c/assets/script.js
@@ -23,44 +23,85 @@ $(function(){
         let button  =   $(this),
             fileName=   button.attr('data-file'),
             fileType=   button.attr('data-type'),
-            path    =   button.closest('.jsImportButtonsWrap').attr('data-action');
+            path    =   button.closest('.jsImportButtonsWrap').attr('data-action'),
+            token   =   '';
         e.preventDefault();
         clearImportLog();
         restartAllButtons();
         setImportLogTitle('Импорт файла '+fileName);
         triggerButtonStatus(button,'process');
         clearFocus();
+        addImportLogRow('Начинаю импорт');
+        addImportLogRow('Получаю токен');
+        alert.show();
+        token   =   getToken(button);
+        console.log(token);
+        if(token!==""){
+            addImportLogRow('Токен получен');
+            $.ajax({
+                type:       'GET',
+                url:        path,
+                data:       {fileName:fileName},
+                cache:      false,
+                dataType:   'json',
+                success:    function(data){
+                    if(data.status==='ok'){
+                        runImport(data.import,token);
+                    }else{
+                        addImportLogRow('Ошибка при копировании файла '+fileName);
+                        addImportLogRow('error Импорт прерван из-за ошибки.');
+                        triggerImportLogStatus('danger');
+                        alert.show();
+                        triggerButtonStatus(button,'error');
+                    }
+                }
+            });
+        }else{
+            addImportLogRow('Токен не получен');
+            addImportLogRow('error Импорт прерван из-за ошибки.');
+            triggerImportLogStatus('danger');
+            triggerButtonStatus(button,'error');
+        }
+    });
+
+    /**
+     * получает токен
+     * @param button
+     * @returns {string}
+     */
+    function getToken(button){
+        let token   =   '';
         $.ajax({
-            type:       'POST',
-            url:        path,
-            data:       {fileName:fileName},
+            type:       'GET',
+            url:        '/bitrix/admin/1c_exchange.php?type=sale&mode=checkauth',
             cache:      false,
-            dataType:   'json',
+            dataType:   'html',
+            async:      false,
             success:    function(data){
-                if(data.status==='ok'){
-                    runImport(data.import);
-                    addImportLogRow('Начинаю импорт');
-                    alert.show();
-                }else{
-                    addImportLogRow('Ошибка при копировании файла '+fileName);
+                if(data.indexOf('Авторизация')>1){
+                    addImportLogRow('Проблема с авторизацией!');
+                    addImportLogRow('Необходимо авторизоваться под пользователем имеющим право на обмен с 1С.');
                     addImportLogRow('error Импорт прерван из-за ошибки.');
                     triggerImportLogStatus('danger');
-                    alert.show();
                     triggerButtonStatus(button,'error');
+                }else{
+                    token   =   data.replace(/success\nPHPSESSID\n.+\nsessid=/gmi,'');
                 }
             }
         });
-    });
+        return  token;
+    }
 
     /**
      * отправляет запросы импорта на сервер
      * @param fileName
+     * @param token
      */
-    function runImport(fileName){
+    function runImport(fileName,token){
         let button  =   bWrap.find('.jsImport[data-file="'+fileName+'"]');
         $.ajax({
             type:       'GET',
-            url:        '/bitrix/admin/1c_exchange.php?type=catalog&mode=import&filename='+fileName,
+            url:        '/bitrix/admin/1c_exchange.php?type=catalog&mode=import&filename='+fileName+'&sessid='+token+'&version=2.08',
             cache:      false,
             dataType:   'html',
             success:    function(data){
@@ -73,7 +114,7 @@ $(function(){
                 }else if(data.indexOf('Импорт успешно завершен')<1){
                     addImportLogRow(data);
                     setTimeout(function(){
-                        runImport(fileName);
+                        runImport(fileName,token);
                     },500);
                 }else{
                     addImportLogRow(data);
